9c12c733a86d709872f037b23a2b4197403ea580
Omkar Kadam <omkarkadam1115@gmail.com>
2025-01-11 14:14:31 +0530

    update javascript

--- Frontend/Home.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('counselingForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-    alert('Your message has been submitted successfully!');
-    this.reset();  // Reset form fields after submission
-});
